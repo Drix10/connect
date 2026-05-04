3a338b16d71883d00901ebcf175029e96ba1d98e
Drishtant Ghosh <ggdrishtant@gmail.com>
2026-05-04 22:40:05 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "@/components/Providers";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: {
@@ -56,6 +57,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body>
         <Providers>{children}</Providers>
+        <Toaster />
       </body>
     </html>
   );
